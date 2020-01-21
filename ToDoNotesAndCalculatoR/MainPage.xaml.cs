92dab3a01a1fd0187cee8a0559b4c3d2d281ba36
omkarvd7066 <omkarvd7066@gmail.com>
2020-01-22 00:38:22 +0530

    Updated All

--- ToDoNotesAndCalculatoR/MainPage.xaml.cs
+++ ToDoNotesAndCalculatoR/MainPage.xaml.cs
@@ -264,7 +264,27 @@ namespace ToDoNotesAndCalculatoR
         private void print_Click(object sender, RoutedEventArgs e)
         {
             for (int x = 0; x < i; x++)
-                listBox1.Items.Add(note[x]);
+            {
+                if (note[x] != "")
+                {
+                    listBox1.Items.Add(note[x]);
+                }
+            }
+
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            for (int x = 0; x < i; x++)
+            {
+                listBox1.Items.Remove(note[x]);
+                note[x] = "";
+            }
 
         }
     }
